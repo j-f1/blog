a163fea87abaa62f3866075a4846b107ddcf183b
Jed Fox <git@jedfox.com>
2020-08-26 11:09:41 -0400

    Update feed.js

--- src/feed.js
+++ src/feed.js
@@ -1,11 +1,13 @@
+const author = { name: "Jed Fox", url: "https://jedfox.com" };
+
 module.exports = (posts) => ({
   version: "https://jsonfeed.org/version/1.1",
   title: "Jed Fox’s Blog",
   home_page_url: "https://blog.jedfox.com",
   feed_url: "https://blog.jedfox.com/feed",
   icon: "https://jedfox.com/favicon-192.png",
-  author: { name: "Jed Fox", url: "https://jedfox.com" },
-  authors: [{ name: "Jed Fox", url: "https://jedfox.com" }],
+  author,
+  authors: [author],
   language: "en-us",
   items: posts.map((post) => ({
     ...post,
