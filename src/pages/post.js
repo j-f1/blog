677725e1f4e1dcfdaedc98a77898a5cf84ec24e7
Jed Fox <git@jedfox.com>
2020-08-27 14:14:54 -0400

    Add a footer to posts with contact information

--- src/pages/post.js
+++ src/pages/post.js
@@ -11,5 +11,20 @@ module.exports = (post) =>
       <h1 class="post-title">${post.title}</h1>
       <p class="post-date">${post.date.toDateString()}</p>
       <article>${safe(post.content_html)}</article>
+      <br />
+      <p style="font-style: italic">
+        Questions or comments? You can DM me on
+        <a href="https://twitter.com/jed_fox1">Twitter</a>,
+        <a href="https://keybase.io/j_f/chat">Keybase</a>, or Discord
+        <span style="font-style: normal">
+          (<code style="font-size: 0.8em">Jed Fox#3082</code>)</span
+        >. You can also send an email to
+        <a
+          href="mailto:jed.twopointzero@gmail.com?subject=${encodeURIComponent(
+            `Comment on “${post.title}”`
+          )}"
+          >jed.twopointzero@gmail.com</a
+        >, optionally using my <a href="/public.pgp">public key</a>.
+      </p>
     `
   );
